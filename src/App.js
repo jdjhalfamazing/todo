61d8730aef1484f292920efeb16124450e8c7259
Joseph Jordan <jordanjoseph25@yahoo.com>
2023-03-16 17:45:38 -0500

    1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+function addTask(){
+const taskInput = document.getElementById('task');
+const taskValue = taskInput.value;
+document.getElementById('taskname').innerHTML = taskValue;
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+    <div id="newtask">
+        <input type="text" id="task" placeholder="Add Tasks"/>
+        <button id="push" onClick={addTask}>Add</button>
     </div>
+    <div id="tasks">
+      <div className="task">
+          <span id="taskname">        
+          </span>
+      </div>
+    </div>
+</div>
   );
 }
 
